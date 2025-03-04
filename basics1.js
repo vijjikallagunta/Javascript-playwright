6764cd746dd284676556dec67f8d6f917f690f4b
vmvlr <vijjikallagunta@gmail.com>
2025-03-04 15:16:01 -0800

    commit1test

---
+++ basics1.js
@@ -0,0 +1,35 @@
+console.log("hi world ") 
+// print the system.println(console.log
+// variable can hold value of any type of data it loosley thighten
+//they can see at run time and datatype it will decide by writing key  word called Var)
+// ES 5 use (var) after ES6 - useing and identifying by using  Let, const
+// difference between let, var, Const
+let a = 4
+console.log(a)
+//what type of data type use typeof
+console.log(typeof(a))
+let b = 24.6
+console.log(typeof(b))
+let c = "vijayinu"
+console.log(typeof(c))
+
+let requied = true
+console.log(typeof(required))
+
+//null and undefined 
+//assignment operators a+b 
+// create and declare 
+// we can deeclare and reassigne with var , but with let only reaaigning is allowed
+// we cannot re declare variable with let keyword but possible with  Var
+c  = a+b// reassigning is allowed with let
+//var c=a+b this is also allowed 
+console.log(c)
+console.log(!required)
+// can not change it assignemnet using const variable
+
+
+
+
+
+//these are comments 
+/**/ 
